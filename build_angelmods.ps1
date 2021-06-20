68da3da95fef261adb55de066a3876f2f1a4db2c
Akaash Suresh <casa.akaash@gmail.com>
2021-06-20 15:30:55 -0500

    Remove zip creation

--- build_angelmods.ps1
+++ build_angelmods.ps1
@@ -1,7 +1,7 @@
 $cwd = Get-Location
 $factorioModFolder = "$($env:APPDATA)\Factorio\mods"
 
-$mods = Get-ChildItem -Directory | Where-Object { $_.BaseName -match "angels" -and $_.BaseName -notmatch "liquidrobot"  } | ForEach-Object { $_.BaseName }
+$mods = Get-ChildItem -Directory | Where-Object { $_.BaseName -match "angels" -and $_.BaseName -notmatch "liquidrobot" } | ForEach-Object { $_.BaseName }
 
 foreach ($mod in $mods)
 {
@@ -10,6 +10,4 @@ foreach ($mod in $mods)
   Get-Childitem -Path ($factorioModFolder) -Filter ("$($info.name)*") | Where-Object { $_.Extension -eq ".zip" -and $_.BaseName.Split("_")[1] -ne $info.version } | ForEach-Object { $_.Delete() }
 
   New-Item -ItemType SymbolicLink -Path ("$($factorioModFolder)\$($info.name)_$($info.version)") -Target ("$($cwd)\$($mod)") -Force
-
-  Compress-Archive -Path "$($cwd)\$($mod)" -Update -DestinationPath "$($factorioModFolder)\$($mod)_$($info.version).zip"
 }
